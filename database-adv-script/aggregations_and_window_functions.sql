8cbfe16504691678138826f5af41bf52fe4fc93d
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-05 19:51:52 +0300

    final fix for aggregations_win_funtions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,35 +4,20 @@ SELECT
 FROM Booking
 GROUP BY user_id;
 
--- 2. Rank properties based on number of bookings using RANK()
--- ===========================================================
-SELECT
-    p.property_id,
-    p.name AS property_name,
-    COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
-FROM
-    Property p
-LEFT JOIN
-    Booking b ON p.property_id = b.property_id
-GROUP BY
-    p.property_id, p.name
-ORDER BY
-    booking_rank;
 
--- ==================================================================
--- 3. Rank properties based on number of bookings using ROW_NUMBER()
--- ==================================================================
+
 SELECT
     p.property_id,
     p.name AS property_name,
-    COUNT(b.booking_id) AS total_bookings,
+    COUNT(b.booking_id) AS total_bookings_received,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank,
     ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number
 FROM
-    Property p
+    Property AS p
 LEFT JOIN
-    Booking b ON p.property_id = b.property_id
+    Booking AS b ON p.property_id = b.property_id
 GROUP BY
-    p.property_id, p.name
+    p.property_id,
+    p.name
 ORDER BY
-    booking_row_number;
+    booking_rank ASC, total_bookings_received DESC; 
