9e993968700c58cc9d10a8261bb1e9bfcf482637
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-05 18:48:03 +0300

    added Join-Query.sql file

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,10 +1,10 @@
 SELECT * FROM Booking 
-INNER JOIN User ON Booking.user_id = User.user_id
+INNER JOIN User ON Booking.user_id = User.user_id;
 
--- property -- review
 
 SELECT * FROM Property 
 LEFT JOIN Review ON property.property_id = Review.property_id
+ORDER BY Property.name
 
 SELECT * FROM User
-FULL OUTER JOIN Booking ON User.user_id = Booking.user_id
+FULL OUTER JOIN Booking ON User.user_id = Booking.user_id;
