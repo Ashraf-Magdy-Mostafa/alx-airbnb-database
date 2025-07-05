aed56acc118c250a574f09aa1c7eb6923bb179a2
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-05 19:12:18 +0300

    added subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,16 @@
+SELECT *
+FROM Property
+WHERE property_id IN (
+    SELECT property_id
+    FROM Review
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+SELECT *
+FROM User 
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking 
+    WHERE Booking.user_id = User.user_id
+) > 3;
